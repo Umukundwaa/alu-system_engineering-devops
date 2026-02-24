edd6dfe510744ac185b5e408474562fa35b9a6bb
Umukundwaa <a.umukundwa1@alustudent.com>
2026-02-24 06:44:53 -0800

    Configure SSH client for key-based authentication
    This Puppet manifest configures the SSH client to use a private key for authentication and disables password authentication.

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,1 +1,17 @@
 
+# Configures SSH client to use private key and refuse password auth
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile',
+}
